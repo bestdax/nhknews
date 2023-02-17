4c6dc7e6b231d7baae304a15b8d72544ad61f9d7
dax <dax26d@gmail.com>
2023-02-17 12:20:41 +0800

    change last part timestamp

--- timestamp.py
+++ timestamp.py
@@ -18,7 +18,7 @@ def gen_ass_timestamp(file):
         end = datetime.utcfromtimestamp(stamp[1] / 1000 + 0.05)
         ass += f'Dialogue: 0,{start.strftime("%H:%M:%S.%f")},{end.strftime("%H:%M:%S.%f")},Default,,0,0,0,,\n'
 
-    ass += "Dialogue: 0,0:04:52.52,0:05:03.00,Default,,0,0,0,,{\fscx192\fscy177\pos(604.308,435.538)}  看到这里的小伙伴动动你的小手点个赞吧 "
+    ass += "Dialogue: 0,0:04:57.52,0:05:03.00,Default,,0,0,0,,{\fscx192\fscy177\pos(604.308,435.538)}  看到这里的小伙伴动动你的小手点个赞吧 "
 
     with open(f'{file.split(".")[0] + ".ass"}', 'w') as f:
         f.write(ass)
